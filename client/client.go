830a05983038c3310e2cf2c635f07ed86f372b39
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-07-31 19:59:04 -0300

    [feat] clent connection

---
+++ client/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to conenct to")
+)
+
+func main() {
+
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+}
